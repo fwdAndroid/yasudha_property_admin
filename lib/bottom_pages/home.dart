d4dfb5f3369fdbe6ac144e2124c22606393a5c61
fwdAndroid <fwdkaleem@gmail.com>
2023-03-02 02:44:21 +0500

    Home Screen Design Complete

--- lib/bottom_pages/home.dart
+++ lib/bottom_pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
@@ -11,6 +12,71 @@ class Home_Screen extends StatefulWidget {
 class _Home_ScreenState extends State<Home_Screen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        title: Text(
+          "Home",
+          style:
+              TextStyle(color: Color(0xff234F68), fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.search,
+                color: Color(0xfff234F68),
+              ))
+        ],
+        backgroundColor: Colors.white,
+      ),
+      body: Column(
+        children: [
+          Image.asset(
+            "assets/logo.png",
+            height: 300,
+            fit: BoxFit.cover,
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(
+                Icons.person,
+                color: Color(0xfff234F68),
+              ),
+              title: Text(
+                "Customer List",
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(
+                Icons.assignment,
+                color: Color(0xfff234F68),
+              ),
+              title: Text(
+                "Agents List",
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(
+                Icons.area_chart,
+                color: Color(0xfff234F68),
+              ),
+              title: Text(
+                "Plot Managment",
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
