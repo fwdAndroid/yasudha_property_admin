e5917457bea4024bc977ba1f68b5899166d8e980
fwdAndroid <fwdkaleem@gmail.com>
2023-03-02 02:47:24 +0500

    Search The Results

--- lib/bottom_pages/search.dart
+++ lib/bottom_pages/search.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
@@ -11,6 +12,34 @@ class Search extends StatefulWidget {
 class _SearchState extends State<Search> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.search,
+                color: Color(0xfff234F68),
+              ))
+        ],
+        backgroundColor: Colors.white,
+      ),
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+              child: Text(
+            "Search Results",
+            style: TextStyle(
+                color: Color(0xfff234F68),
+                fontWeight: FontWeight.bold,
+                fontSize: 25),
+          ))
+        ],
+      ),
+    );
   }
 }
